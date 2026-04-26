0483ff43fcfc9036cdcb24a2bbcd5aa66a40ec8f
Detach0-0 <detach0-0@outlook.com>
2026-04-26 10:09:02 +0800

    refactor: stabilize editor bridge protocol

---
+++ Dev/Typedown.Core/Services/EditorBridge.cs
@@ -0,0 +1,90 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
+using Typedown.Core.Interfaces;
+using Typedown.Services;
+
+namespace Typedown.Core.Services
+{
+    public class EditorBridge : IEditorBridge
+    {
+        private readonly RemoteInvoke remoteInvoke;
+
+        private readonly Transport transport;
+
+        private readonly Func<string, bool> sender;
+
+        public EditorBridge(RemoteInvoke remoteInvoke, Transport transport, Func<string, bool> sender)
+        {
+            this.remoteInvoke = remoteInvoke ?? throw new ArgumentNullException(nameof(remoteInvoke));
+            this.transport = transport ?? throw new ArgumentNullException(nameof(transport));
+            this.sender = sender ?? throw new ArgumentNullException(nameof(sender));
+        }
+
+        public bool Send(string name, object args)
+        {
+            var payload = JsonConvert.SerializeObject(new { name, args }, Core.Config.EditorJsonSerializerSettings);
+            return sender(payload);
+        }
+
+        public async Task ReceiveAsync(string json)
+        {
+            EditorMessage msg;
+            try
+            {
+                msg = JsonConvert.DeserializeObject<EditorMessage>(json, Core.Config.EditorJsonSerializerSettings);
+            }
+            catch
+            {
+                return;
+            }
+
+            if (msg == null || string.IsNullOrWhiteSpace(msg.Type))
+                return;
+
+            switch (msg.Type)
+            {
+                case "invoke":
+                    await HandleInvokeAsync(msg);
+                    break;
+                case "message":
+                    transport.EmitMessage(msg.Name, msg.Args);
+                    break;
+                case "diffmsg":
+                    transport.EmitDiffMessage(msg.Name, msg.Args, msg.Diff, msg.Start, msg.End);
+                    break;
+            }
+        }
+
+        private async Task HandleInvokeAsync(EditorMessage msg)
+        {
+            try
+            {
+                var ret = await remoteInvoke.Invoke(msg.Name, msg.Args);
+                Send(msg.Id, new { code = 0, data = ret });
+            }
+            catch (Exception ex)
+            {
+                Send(msg.Id, new { code = 1, msg = ex.Message });
+            }
+        }
+
+        private sealed class EditorMessage
+        {
+            public string Id { get; set; }
+
+            public string Name { get; set; }
+
+            public JToken Args { get; set; }
+
+            public string Type { get; set; }
+
+            public bool Diff { get; set; }
+
+            public int Start { get; set; }
+
+            public int End { get; set; }
+        }
+    }
+}
--- Dev/Typedown.Core/Services/Transport.cs
+++ Dev/Typedown.Core/Services/Transport.cs
@@ -1,8 +1,5 @@
-﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
 using System.Collections.Generic;
-using Typedown.Core.Interfaces;
 using Typedown.Core.Models;
 using Typedown.Core.Services;
 
@@ -12,56 +9,25 @@ namespace Typedown.Services
     {
         private readonly Dictionary<string, string> prevDic = new();
 
-        public IServiceProvider ServiceProvider { get; }
-
-        public RemoteInvoke RemoteInvoke { get; }
-
         public EventCenter EventCenter { get; }
 
         public Transport(RemoteInvoke remoteInvoke, EventCenter eventCenter)
         {
-            RemoteInvoke = remoteInvoke;
             EventCenter = eventCenter;
         }
 
-        public async void EmitWebViewMessage(IMarkdownEditor sender, string json)
+        public void EmitMessage(string name, JToken args)
         {
-            var msg = JsonConvert.DeserializeObject<EditorMessage>(json, Core.Config.EditorJsonSerializerSettings);
-            switch (msg.Type)
-            {
-                case "invoke":
-                    try
-                    {
-                        var ret = await RemoteInvoke.Invoke(msg.Name, msg.Args);
-                        sender.PostMessage(msg.Id, new { code = 0, data = ret });
-                    }
-                    catch (Exception ex)
-                    {
-                        sender.PostMessage(msg.Id, new { code = 1, msg = ex.Message });
-                    }
-                    break;
-                case "message":
-                    EventCenter.EmitEvent(msg.Name, new EditorEventArgs(msg.Name, msg.Args));
-                    break;
-                case "diffmsg":
-                    if (msg.Diff)
-                        prevDic[msg.Name] = prevDic[msg.Name].Substring(0, msg.Start) + msg.Args + prevDic[msg.Name].Substring(msg.End);
-                    else
-                        prevDic[msg.Name] = msg.Args.ToString();
-                    EventCenter.EmitEvent(msg.Name, new EditorEventArgs(msg.Name, JToken.Parse(prevDic[msg.Name])));
-                    break;
-            }
+            EventCenter.EmitEvent(name, new EditorEventArgs(name, args));
         }
 
-        public class EditorMessage
+        public void EmitDiffMessage(string name, JToken args, bool diff, int start, int end)
         {
-            public string Id { get; set; }
-            public string Name { get; set; }
-            public JToken Args { get; set; }
-            public string Type { get; set; }
-            public bool Diff { get; set; }
-            public int Start { get; set; }
-            public int End { get; set; }
+            if (diff && prevDic.TryGetValue(name, out var prev))
+                prevDic[name] = prev.Substring(0, start) + args + prev.Substring(end);
+            else
+                prevDic[name] = args?.ToString() ?? "null";
+            EventCenter.EmitEvent(name, new EditorEventArgs(name, JToken.Parse(prevDic[name])));
         }
     }
 }
